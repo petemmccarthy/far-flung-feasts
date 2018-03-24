14ebb68100b689acb00a8b65d187286393b6ccce
Peter McCarthy <pete@peters-mbp.home>
2018-03-24 18:39:07 +0000

    tests for countries and minor layout changes

--- tests/acceptance/far-flung-feasts-test.js
+++ tests/acceptance/far-flung-feasts-test.js
@@ -16,4 +16,15 @@ module('Acceptance | far flung feasts', function(hooks) {
     assert.equal(currentURL(), '/about', 'takes the user to the about page');
   });
 
+  test('visiting the countries page', async function(assert) {
+    await visit('/');
+    await click(".menu-countries");
+    assert.equal(currentURL(), '/countries', 'takes the user to the countries page');
+  });
+
+  test('should list available countries', async function(assert) {
+    await visit('/countries');
+    assert.equal(this.element.querySelectorAll('.flag').length, 3, 'should display 3 country flags');
+  })
+
 });

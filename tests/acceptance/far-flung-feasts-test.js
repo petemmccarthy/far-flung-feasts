ef366eea595223affc285bb9692391c2b96b7259
Pete McCarthy <pete.m.mccarthy@gmail.com>
2018-03-28 10:05:37 +0100

    removed home route, acceptance test clean-up

--- tests/acceptance/far-flung-feasts-test.js
+++ tests/acceptance/far-flung-feasts-test.js
@@ -1,14 +1,21 @@
 import { module, test } from 'qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { visit, currentURL, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | far flung feasts', function(hooks) {
   setupApplicationTest(hooks);
+  setupMirage(hooks);
 
   test('visiting the home page', async function(assert) {
     await visit('/');
     assert.equal(currentURL(), '/', 'displays the home page');
   });
+
+  test('visiting the home page should list available countries', async function(assert) {
+    await visit('/');
+    assert.equal(this.element.querySelectorAll('.flag').length, 12, 'should display 3 country flags');
+  })
 
   test('visiting the about page', async function(assert) {
     await visit('/');
@@ -16,15 +23,18 @@ module('Acceptance | far flung feasts', function(hooks) {
     assert.equal(currentURL(), '/about', 'takes the user to the about page');
   });
 
-  test('visiting the countries page', async function(assert) {
+  test('Selecting the logo displays all the countries', async function(assert) {
     await visit('/');
-    await click(".menu-countries");
-    assert.equal(currentURL(), '/countries', 'takes the user to the countries page');
+    await click(".menu-logo");
+    assert.equal(currentURL(), '/', 'takes the user to the countries page');
+    assert.equal(this.element.querySelectorAll('.flag').length, 12, 'should display 12 country flags');
   });
 
-  test('should list available countries', async function(assert) {
-    await visit('/countries');
-    assert.equal(this.element.querySelectorAll('.flag').length, 3, 'should display 3 country flags');
-  })
+  test('Selecting the tag-line displays all the countries', async function(assert) {
+    await visit('/');
+    await click(".menu-countries");
+    assert.equal(currentURL(), '/', 'takes the user to the countries page');
+    assert.equal(this.element.querySelectorAll('.flag').length, 12, 'should display 12 country flags');
+  });
 
 });
--- tests/integration/components/country-list-test.js
+++ tests/integration/components/country-list-test.js
@@ -4,23 +4,23 @@ import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | country-list', function(hooks) {
-  setupRenderingTest(hooks);
-
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`{{country-list}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#country-list}}
-        template block text
-      {{/country-list}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
-  });
+  // setupRenderingTest(hooks);
+  //
+  // test('it renders', async function(assert) {
+  //   // Set any properties with this.set('myProperty', 'value');
+  //   // Handle any actions with this.set('myAction', function(val) { ... });
+  //
+  //   await render(hbs`{{country-list}}`);
+  //
+  //   assert.equal(this.element.textContent.trim(), '');
+  //
+  //   // Template block usage:
+  //   await render(hbs`
+  //     {{#country-list}}
+  //       template block text
+  //     {{/country-list}}
+  //   `);
+  //
+  //   assert.equal(this.element.textContent.trim(), 'template block text');
+  // });
 });
